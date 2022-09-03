7026d682ce62515be8a7cf6d4094ae882bd8539c
Kevison Pascoal Mendes <kevison.pascoal@gmail.com>
2022-09-03 19:48:03 -0300

    switch:zap:

---
+++ 6-switch.dart
@@ -0,0 +1,20 @@
+void main() {
+  String linguagem = "JavaScript";
+
+  switch(linguagem) {
+    case "Dart":
+      print("Dart!!");
+      break;
+    case "Java":
+      print("Java");
+      break;
+    case "Swift":
+      print("Swift");
+      break;
+    case "JavaScript":
+      print("JAVASCRIPT MELHOR DO MUNDO!");
+      break;  
+    default:
+      print("Outra...");      
+  }
+}
